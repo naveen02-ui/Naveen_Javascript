bc4ac9b3b142ed8c11baaee422a892cb34e0ffe2
Naveen Suman <ns7144007@gmail.com>
2026-05-17 14:47:35 +0000

    comparison

---
+++ JavaScipt_Programming/01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+//*****Comparisons******//
+
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log( null > 0);
+console.log( null == 0);
+console.log( null >= 0);  // this is not fact this is language conversion
+console.log( null < 0);  // We avoid this
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);  // Always false
+console.log(undefined < 0); // We avoid this
+
+console.log("2" == 2);
+
+
+
