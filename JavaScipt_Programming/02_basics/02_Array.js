baf0f0755ff2c100e1d8c22f5b277b00e6124b22
Naveen Suman <ns7144007@gmail.com>
2026-05-25 07:45:40 +0000

    May 25, 2026, 7:45 AM

--- JavaScipt_Programming/02_basics/02_Array.js
+++ JavaScipt_Programming/02_basics/02_Array.js
@@ -1,7 +1,8 @@
 const marvel_heroes = ["thor", 'ironman', 'hulk', 'captain america']
 const dc_heroes = ['superman', 'batman',]
 
-marvel_heroes.push('dc_heroes') // it will add dc heroes to the marvel heroes
+marvel_heroes.push(dc_heroes) // it will add dc heroes to the marvel heroes
 console.log(marvel_heroes);
 
-console.log(marvel_heroes[0][2]);
+console.log(marvel_heroes[1][2]);
+
