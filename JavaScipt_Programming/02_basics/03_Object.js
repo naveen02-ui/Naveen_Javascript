6278c3282d16600984656b4152443fbdd81be727
Naveen Suman <ns7144007@gmail.com>
2026-05-25 08:37:06 +0000

    May 25, 2026, 8:37 AM

--- JavaScipt_Programming/02_basics/03_Object.js
+++ JavaScipt_Programming/02_basics/03_Object.js
@@ -1,4 +1,25 @@
 // singleton
 // object.create
 // object literals
-const JSuser = {}
+
+const mySym = Symbol("key1")
+const JSuser = {
+    name: "Naveen",
+    "Full name": "Naveen Suman",
+    [mySym]: "myKey1",
+    age: 21,
+    location: "India",
+    email: "naveen@example.com",
+    isLoggesIn: true,
+    lastLoginDays: ["Monday", "Tuesday", "Wednesday"],
+}
+console.log(JSuser.name);
+console.log(JSuser["name"]) // both are same but second one is used when we want to access property with space or special character
+console.log(JSuser["Full name"]); // it is used to access property with space or special character
+console.log(JSuser[mySym]); // Important case to access symbol property
+
+JSuser.email = "naveen@google.com"
+object.freeze(JSuser) // it will freeze the object and we cannot change the properties of the object
+JSuser.email = "naveen@microsoft.com"
+console.log(JSuser);
+
